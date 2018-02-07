ef1e2f3e2b1ea3021b9e1b8e7e96d176f8acad7f
César M. Cristóbal <cmartin@bitban.com>
2018-02-07 19:32:39 +0100

    check mk module auto_activate_changes and auto_discover_services in add_host functionality

--- lib/ansible/modules/monitoring/check_mk/check_mk_add_host.py
+++ lib/ansible/modules/monitoring/check_mk/check_mk_add_host.py
@@ -1,6 +1,8 @@
 import re
 from ansible.modules.monitoring.check_mk.check_mk_web_api import CheckMKWebApi
 from ansible.modules.monitoring.check_mk.check_mk_params_helper import CheckMKParamsHelper
+from ansible.modules.monitoring.check_mk.check_mk_activate_changes import ActivateChanges
+from ansible.modules.monitoring.check_mk.check_mk_discover_services import DiscoverServices
 
 class AddHost:
 
@@ -9,6 +11,7 @@ class AddHost:
                                  module.params["username"],
                                  module.params["password"],
                                  module)
+        self.module = module
 
 
     def get_result(self, params):
@@ -20,15 +23,25 @@ class AddHost:
 
         if response["result_code"] == 0:
             response.update({"changed": True})
+
+            if params.has_key("auto_activate_changes") or params.has_key("auto_discover_services"):
+                return self.discover_activate(response, params)
+
             return response
+
         if response["result_code"] == 1:
 
             regex = re.compile("Check_MK exception: Host .* already exists in the folder servers")
 
             if regex.match(response["result"]):
                 response.update({"changed": False})
+
+                if params.get("auto_activate_changes") or params.get("auto_discover_services"):
+                    return self.discover_activate(response, params)
+
                 return response
 
+            return {"failed": True, "msg": response["result"]}
 
     def get_data(self, params):
 
@@ -40,3 +53,17 @@ class AddHost:
         ret.update(CheckMKParamsHelper(params).get_optional_params(["folder", "attributes", "create_folders"]))
 
         return ret
+
+    def discover_activate(self, response, params):
+
+        if params.get("auto_discover_services"):
+            discover = DiscoverServices(self.module).get_result(params)
+            if discover.has_key("failed"):
+                return discover
+
+        if params.get("auto_activate_changes"):
+            activate = ActivateChanges(self.module).get_result(params)
+            if activate.has_key("failed"):
+                return activate
+
+        return response
